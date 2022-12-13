bb2cf772ff3d4bb5989bce4e86190960285bc7a9
erhan basaran <e.basaran.111@gmail.com>
2022-12-13 19:08:34 +0100

    Bug-fix

--- frontend/angular-app/src/app/app.module.ts
+++ frontend/angular-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
@@ -24,6 +23,6 @@ import {FormsModule} from "@angular/forms";
         MatIconModule
     ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AdminComponent]
 })
 export class AppModule { }
